b0a2a0257b635c4fd10665f0ee2096d283702506
Richie735 <93219607+Richie735@users.noreply.github.com>
2023-11-11 18:02:48 +0000

    Add DB Connection with Mongoose
    - Establish connection
    - Install Mongoose package for MongoDB integration

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,12 +1,17 @@
 "use server";
 
+import { connectToDatabase } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 
 export async function scrapeAndSaveProduct(productUrl: string) {
    if (!productUrl) return;
 
    try {
+      connectToDatabase();
+
       const scrapedProduct = await scrapeAmazonProduct(productUrl);
+
+      if (!scrapedProduct) return;
    } catch (error: any) {
       throw new Error("Failed to create/update product: ${error.message}");
    }
