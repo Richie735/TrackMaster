dc8a887062e752508f70367f9c79c4feb8b99775
Richie735 <93219607+Richie735@users.noreply.github.com>
2023-11-12 20:12:46 +0000

    Fix Build Errors pt.2

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -120,7 +120,7 @@ const ProductDetails = async ({ params: { id } }: Props) => {
                   </div>
                </div>
 
-               <Modal />
+               <Modal productId={id} />
             </div>
          </div>
 
